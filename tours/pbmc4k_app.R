dec969579be797ab6fe8070e1657f147edec26e1
Federico Marini <marinif@uni-mainz.de>
2018-05-02 20:27:20 +0200

    renamed the pbmc4k part

--- tours/pbmc4k_app.R
+++ tours/pbmc4k_app.R
@@ -8,7 +8,7 @@ sce <- readRDS("sce.rds")
 # Setting up defaults for all panels.
 redDim <- DataFrame(Type="TSNE", ColorBy="Column data", ColorByColData="Detection")
 colData <- DataFrame(YAxis="log10_total_counts", XAxis="Column data", XAxisColData="Cluster", ColorBy="Column data", ColorByColData="Detection")
-featExpr <- DataFrame(YAxisFeatName="PF4", XAxis="Column data", XAxisColData="Cluster", ColorBy="Column data", ColorByColData="log10_total_counts")
+featAssay <- DataFrame(YAxisFeatName="PF4", XAxis="Column data", XAxisColData="Cluster", ColorBy="Column data", ColorByColData="log10_total_counts")
 heatMap <- DataFrame(FeatName=I(list(c("PF4", "PPBP", "TMSB4X", "B2M", "ACTB", "SDPR"))), 
                      ColData=I(list(c("Cluster"))), Lower=-5, Upper=5)
 rowStat <- DataFrame(Search="PF4", Selected="PF4")
@@ -18,10 +18,10 @@ colData$SelectByPlot <- "Reduced dimension plot 1"
 colData$SelectEffect <- "Transparent"
 colData$SelectAlpha <- 0
 
-featExpr$SelectByPlot <- "Reduced dimension plot 1"
-featExpr$SelectEffect <- "Transparent"
-featExpr$SelectAlpha <- 0
-featExpr$YAxisRowTable <- "Row statistics table 1"
+featAssay$SelectByPlot <- "Reduced dimension plot 1"
+featAssay$SelectEffect <- "Transparent"
+featAssay$SelectAlpha <- 0
+featAssay$YAxisRowTable <- "Row statistics table 1"
 
 heatMap$SelectByPlot <- "Reduced dimension plot 1"
 
@@ -32,7 +32,7 @@ annot.fun <- annotateEnsembl(sce, orgdb=org.Hs.eg.db, keytype="ENSEMBL", rowdata
 # Setting up the initial panels.
 init <- DataFrame(Name=c("Reduced dimension plot 1",
                          "Column data plot 1",
-                         "Feature expression plot 1",
+                         "Feature assay plot 1",
                          "Row statistics table 1",
                          "Heat map 1"),
                   Width=c(4,4,4,8,4))
@@ -41,5 +41,5 @@ init <- DataFrame(Name=c("Reduced dimension plot 1",
 tour <- read.delim("pbmc4k_tour.txt", header=TRUE, sep=";", comment.char="", quote="")
 
 # Running the app.
-iSEE(sce, redDimArgs=redDim, colDataArgs=colData, featExprArgs=featExpr, heatMapArgs=heatMap, rowDataMax=0, rowStatArgs=rowStat,
+iSEE(sce, redDimArgs=redDim, colDataArgs=colData, featAssayArgs=featAssay, heatMapArgs=heatMap, rowDataMax=0, rowStatArgs=rowStat,
     appTitle="Running emptyDrops on the PBMC 4K dataset", initialPanels=init, annotFun=annot.fun, tour=tour, runLocal=FALSE)
