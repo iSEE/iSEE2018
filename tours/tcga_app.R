e0f3c6c3e6c22652b71ed21b5e4c21dceedc639b
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-05-02 22:58:47 +0100

    move TCGA data-related preprocessing to _data.R

--- tours/tcga_app.R
+++ tours/tcga_app.R
@@ -1,38 +1,19 @@
 
-# User inputs ----
+# Tour steps ----
 
 tour_steps <- "tcga_tour.txt"
 stopifnot(file.exists(tour_steps))
+tour <- read.delim(tour_steps, sep=";", stringsAsFactors = FALSE,row.names = NULL)
 
 # Libraries ----
 
 stopifnot(require(iSEE))
-stopifnot(requireNamespace("ExperimentHub"))
-stopifnot(requireNamespace("scater"))
-stopifnot(requireNamespace("irlba"))
-stopifnot(requireNamespace("Rtsne"))
-
-# Preprocessing ----
-
-# Vignette demo
-
-ehub <- ExperimentHub::ExperimentHub()
-eh1 <- ehub[["EH1"]] # an ExpressionSet
-se1 <- as(eh1, "SummarizedExperiment")
-sce <- as(se1, "SingleCellExperiment")
-sce <- scater::runPCA(sce, exprs_values = "exprs")
-irlba_out <- irlba::irlba(assay(sce, "exprs"))
-tsne_out <- Rtsne::Rtsne(
-  irlba_out$v, pca = FALSE, perplexity = 50, verbose = TRUE)
-reducedDim(sce, "TSNE") <- tsne_out$Y
-
-# library size and CPM
-
-colData(sce)[,"lib_size"] <- colSums(assay(sce, "exprs"))
-assay(sce, "log2CPM") <-
-  log2(1 + t(t(assay(sce, "exprs")) / colData(sce)[,"lib_size"]))
 
 # Application setup ----
+
+if (length(ls(pattern = "sce")) != 1){
+  stop("Please run tcga_data.R first to generate the `sce` object.")
+}
 
 # Panel 1: colData (phenotype selection)
 # Y = CancerType
@@ -125,8 +106,6 @@ initialPanels = DataFrame(
       5 # featAssays
     )
   )
-
-tour <- read.delim(tour_steps, sep=";", stringsAsFactors = FALSE,row.names = NULL)
 
 app <- iSEE(
   sce, tour = tour,
---
+++ tours/tcga_data.R
@@ -0,0 +1,28 @@
+
+# Librarires ----
+
+stopifnot(requireNamespace("ExperimentHub"))
+stopifnot(require(SingleCellExperiment))
+stopifnot(requireNamespace("scater"))
+stopifnot(requireNamespace("irlba"))
+stopifnot(requireNamespace("Rtsne"))
+
+# Preprocessing ----
+
+# Vignette demo
+
+ehub <- ExperimentHub::ExperimentHub()
+eh1 <- ehub[["EH1"]] # an ExpressionSet
+se1 <- as(eh1, "SummarizedExperiment")
+sce <- as(se1, "SingleCellExperiment")
+sce <- scater::runPCA(sce, exprs_values = "exprs")
+irlba_out <- irlba::irlba(assay(sce, "exprs"))
+tsne_out <- Rtsne::Rtsne(
+  irlba_out$v, pca = FALSE, perplexity = 50, verbose = TRUE, max_iter = 100)
+reducedDim(sce, "TSNE") <- tsne_out$Y
+
+# library size and CPM
+
+colData(sce)[,"lib_size"] <- colSums(assay(sce, "exprs"))
+assay(sce, "log2CPM") <-
+  log2(1 + t(t(assay(sce, "exprs")) / colData(sce)[,"lib_size"]))
