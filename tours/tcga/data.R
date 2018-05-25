194b792c659df7019b02e703bdf8e5a0d2d3f9a1
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-05-25 23:15:44 +0100

    run pca on log-transformed counts

--- tours/tcga/data.R
+++ tours/tcga/data.R
@@ -11,20 +11,20 @@ eh1 <- ehub[["EH1"]] # an ExpressionSet
 se1 <- as(eh1, "SummarizedExperiment")
 sce <- as(se1, "SingleCellExperiment")
 
-# Vignette demo
-
-set.seed(12321)
-sce <- runPCA(sce, exprs_values = "exprs")
-irlba_out <- irlba(assay(sce, "exprs"))
-tsne_out <- Rtsne(irlba_out$v, pca = FALSE, perplexity = 50)
-reducedDim(sce, "TSNE") <- tsne_out$Y
-
 # library size and CPM
 
 colData(sce)[,"lib_size"] <- colSums(assay(sce, "exprs"))
 assay(sce, "log2CPM") <-
   log2(1 + t(t(assay(sce, "exprs")) / colData(sce)[,"lib_size"]))
 
+# Dimensionality reduction
+
+set.seed(12321)
+sce <- runPCA(sce, exprs_values = "log2CPM")
+irlba_out <- irlba(assay(sce, "exprs"))
+tsne_out <- Rtsne(irlba_out$v, pca = FALSE, perplexity = 50, verbose = TRUE)
+reducedDim(sce, "TSNE") <- tsne_out$Y
+
 # Saving the results.
 
-saveRDS(file="sce.rds", sce)  
+saveRDS(file="sce.rds", sce)
