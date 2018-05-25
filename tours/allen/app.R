eee7d8211eb7bb8dbce3e04a7a10602cba542bc2
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-05-25 19:01:39 +0100

    Edited allen files for consistency.

--- tours/allen/app.R
+++ tours/allen/app.R
@@ -1,9 +1,12 @@
 library(iSEE)
-# read in the data and the tour
-sce <- readRDS("allen_sce.rds")
+
+# Read in the data and the tour
+sce <- readRDS("sce.rds")
 tour <- read.delim(system.file("extdata", "intro_firststeps.txt",package = "iSEE"),
-                   sep=";", stringsAsFactors = FALSE,row.names = NULL)
-# edit step1 to give a note on how to restart the tour
-tour$intro[1] <- paste0(tour$intro[1],"<br/><br/><b>NOTE:</b> if the tour doesn't run properly at first, exit and click on the top-right question mark to start a new tour.")
-# launch the app
+    sep=";", stringsAsFactors = FALSE,row.names = NULL)
+
+# Edit step 1 to give a note on how to restart the tour
+tour$intro[1] <- paste0(tour$intro[1], '<br/><br/><font color="##FF0000"><strong>NOTE:</strong></font> if the tour is not jumping to the correct UI elements, exit by clicking anywhere else on the app, and click on the top-right question mark to start a new tour.')
+
+# Launch the app
 iSEE(sce,tour = tour,appTitle = "iSEE - demo on the allen dataset")
