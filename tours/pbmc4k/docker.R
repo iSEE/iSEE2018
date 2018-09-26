d3418c6e4461813b4870ef5011304ca03dabace7
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-09-26 22:36:34 +0100

    do not install the package at runtime; rely on the iSEE container to provide it

--- tours/pbmc4k/docker.R
+++ tours/pbmc4k/docker.R
@@ -1,9 +1,6 @@
 timestamp()
 message("* Started")
 
-# The container does not include this package yet; it must be installed at runtime
-BiocManager::install("org.Hs.eg.db")
-
 out <- source("app.R")
 shiny::runApp(out$value, port = 1234, host = "0.0.0.0")
 
